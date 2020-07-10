f1b08260883e56e56bed8bb4005033142845f505
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-10 19:09:12 +0300

    2 func for getting and posting goals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  const courseGoals = [
+    { id: 'cds', text: 'finish' },
+    { id: 'cdsx', text: 'finish' },
+    { id: 'cdsz', text: 'finish' }
+  ];
+
+  const addNewGoalHandler = (newGoal)=>{
+    courseGoals.push(newGoal);
+  };
+
+  return (
+    <div className="ma">
+      <h1>Goals for course</h1>
+      <NewGoal onAddGoal={addNewGoalHandler} />
+      <GoalList goals={courseGoals} />
+    </div>
+
+  );
 };
 
 export default App;
