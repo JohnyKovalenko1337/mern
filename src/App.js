3b7281f7bc4e340341dea8ac6abf29ea63021277
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-11 21:59:29 +0300

    added main navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,  Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+        </Switch>
+      </main>
     </Router>
   );
 }
