d3a73b46e0595d542e8471ba96d95e8634febdb9
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-11 21:57:43 +0300

    added dummy users handling

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UserList from '../components/UserList';
 
-const Users = ()=>{
-    return <h2>Users works</h2>
+const Users = () => {
+    const USERS = [
+        {
+            id:'u1',
+            name:'Elle',
+            image:'https://i.pinimg.com/originals/92/c2/f0/92c2f03407ee7bc8dab7c2962388a139.jpg',
+            places:3
+        }
+    ];
+
+    return (
+        <UserList items={USERS} />
+    );
 };
 export default Users;
