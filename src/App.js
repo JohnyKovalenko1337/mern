a344f74305413f22a5ee21aebd45fcc286900f9b
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-11 10:34:31 +0300

    starting proj

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from 'react';
-import './App.css';
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
 
-const App = () => {
-  const courseGoals = [
-    { id: 'cds', text: 'finish' },
-    { id: 'cdsx', text: 'finish' },
-    { id: 'cdsz', text: 'finish' }
-  ];
-
-  const addNewGoalHandler = (newGoal)=>{
-    courseGoals.push(newGoal);
-  };
-
-  return (
-    <div className="ma">
-      <h1>Goals for course</h1>
-      <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList goals={courseGoals} />
-    </div>
-
-  );
-};
+function App() {
+  return <h1>Let's start!</h1>;
+}
 
 export default App;
