c34d3b51bf6761ab5ba7cbd0200109102bc8e0b0
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-11 13:00:49 +0300

    starting routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route,  Switch } from 'react-router-dom'
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 
