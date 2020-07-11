8d687c2f51279a23c0e3e380a94db1b452945d4e
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-11 12:59:48 +0300

    added router + switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
-function App() {
-  return <h1>Let's start!</h1>;
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new" exact>
+        <NewPlace />
+      </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
