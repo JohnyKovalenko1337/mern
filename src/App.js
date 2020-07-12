e5710af4e1d76a6617add2fb98713c7297be2f3d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-12 14:51:46 +0300

    added userPlaces route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
 import Users from './user/pages/Users';
+import UserPlaces from './places/pages/UserPlaces';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/navigation/MainNavigation';
 
@@ -14,6 +15,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
