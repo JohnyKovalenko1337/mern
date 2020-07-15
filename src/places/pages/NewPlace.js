38575bf369a55b30cb5bb2fc402a589c6208fe4d
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-15 22:23:12 +0300

    added descrip input

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../util/validators';
 import './NewPlace.css';
 
-const Places = ()=>{
+const Places = () => {
+
+    const titleInputHandler = useCallback((id, value, isValid) =>{
+
+    }, [
+        
+    ]);
+
+    const descriptionInputHandler = useCallback((id, value, isValid) =>{
+
+    }, [
+        
+    ]);
+
     return (
         <form className="place-form">
-            <Input element={'input'} type="text" label="Title" errorText="plz enter a valid title" /> {/*validators={[]}  onChange={}*/ }
+            <Input
+                element={'input'}
+                type="text"
+                label="Title"
+                id="title"
+                errorText="plz enter a valid title"
+                validators={[VALIDATOR_REQUIRE()]}
+                onInput={titleInputHandler}
+            /> 
+            <Input
+                element={'textarea'}
+                label="Description"
+                id="description"
+                errorText="plz enter a valid description,(min 5 elements)"
+                validators={[VALIDATOR_REQUIRE(), VALIDATOR_MINLENGTH(5)]}
+                onInput={descriptionInputHandler}
+            /> 
         </form>
-        )
+    )
 };
 export default Places;
