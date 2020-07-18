6e15bde9cd3cc5f6b94adec6b281e68edd39eb5f
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-18 13:09:08 +0300

    added context for navigation Links

--- src/shared/components/navigation/NavLinks.js
+++ src/shared/components/navigation/NavLinks.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 
-
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css'
 
-const NavLinks = (props)=>{
+const NavLinks = (props) => {
+    const auth = useContext(AuthContext);
+
     return (
         <ul className='nav-links '>
             <li>
@@ -12,21 +14,34 @@ const NavLinks = (props)=>{
                     All Users
                 </NavLink>
             </li>
-            <li>
-                <NavLink to="/u1/places">
-                    My Places
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/u1/places">
+                        My Places
                 </NavLink>
-            </li>
-            <li>
-                <NavLink to="/places/new">
-                    New Place
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/places/new">
+                        ADD Place
                 </NavLink>
-            </li>
-            <li>
-                <NavLink to="/auth">
-                    Authenticate
+                </li>
+            )}
+            {!auth.isLoggedIn && (
+                <li>
+                    <NavLink to="/auth">
+                        Authenticate
                 </NavLink>
-            </li>
+                </li>
+            )}
+            {auth.isLoggedIn && (
+                <li>
+                    <button onClick={auth.logout}>
+                        LOGOUT
+                    </button>
+                </li>
+            )}
         </ul>
     );
 };
