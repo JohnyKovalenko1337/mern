10d1e4a9c7a1805f5b411d8579ab10e086b53eef
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-22 12:05:16 +0300

    added fetching users from backend

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,19 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import UserList from '../components/UserList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-    const USERS = [
-        {
-            id:'u1',
-            name:'Elle',
-            image:'https://i.pinimg.com/originals/92/c2/f0/92c2f03407ee7bc8dab7c2962388a139.jpg',
-            places:3
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState();
+    const [data, setData] = useState();
+    useEffect(() => {
+        const sendRequest = async () => {
+            setIsLoading(true);
+            try {
+
+                const responce = await fetch('http://localhost:8000/user');
+
+                const responceData = await responce.json();
+
+                if (!responce.ok) {
+                    throw new Error(responceData.message);
+                }
+
+                setData(responceData.users);
+            }
+            catch (err) {
+                setError(err.message || 'An error has occurred');
+            }
+            setIsLoading(false);
+
         }
-    ];
+        sendRequest();
+    }, []);
+
+    const handelError = () => {
+        setError(null);
+    };
 
     return (
-        <UserList items={USERS} />
+        <React.Fragment>
+            <ErrorModal error={error} onClear={handelError} /> {/* setting error from useState */}
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner asOverlay />
+                </div>)
+            }
+
+            {!isLoading && data && <UserList items={data} />}
+
+        </React.Fragment>
+
     );
 };
 export default Users;
