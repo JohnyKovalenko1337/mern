5d90996351a3d99319b4a1e1822da9745545cfae
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-23 09:17:45 +0300

    added userId state for context

--- src/App.js
+++ src/App.js
@@ -12,13 +12,16 @@ import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState();
 
-  const login = useCallback(() => {
+  const login = useCallback((uid) => {
     setIsLoggedIn(true);
+    setUserId(uid);
   }, [])
 
   const logout = useCallback(() => {
     setIsLoggedIn(false);
+    setUserId(null);
   }, [])
 
   let routes;
@@ -64,6 +67,7 @@ const App = () => {
     <AuthContext.Provider value={
       {
         isLoggedIn: isLoggedIn,
+        userId: userId,
         login: login,
         logout: logout
       }
