7cb182b30b068f22f82e98661e1ce4ee9ec556c2
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-07-23 09:18:29 +0300

    added logic to load places of user from backend

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,42 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 import PlacesList from '../components/PlacesList';
-const Dummy_Items = [
-      {
-           id:'p1',
-           title:'Louvre',
-           imageUrl:'https://s.france24.com/media/display/ffb00d5c-5bcb-11ea-9b68-005056a98db9/w:1280/p:16x9/5ebdce7c4db36aa769d6edb94f5b288f18ac266c.webp',
-           address:'Musée du Louvre, Cour Napoléon et Pyramide du Louvre, 75001 Paris',
-           description:'One of the famoust museum',
-           location:{
-               lat:48.8613684,
-               lng:2.3254948
-           },
-           creator:'u1',
-       },
-       {
-           id:'p1',
-           title:'Louvre',
-           imageUrl:'https://s.france24.com/media/display/ffb00d5c-5bcb-11ea-9b68-005056a98db9/w:1280/p:16x9/5ebdce7c4db36aa769d6edb94f5b288f18ac266c.webp',
-           address:'Musée du Louvre, Cour Napoléon et Pyramide du Louvre, 75001 Paris',
-           description:'One of the famoust museum',
-           location:{
-               lat:48.8613684,
-               lng:2.3254948
-           },
-           creator:'u2',
-       }
-]
+
 
 const UserPlaces = (props) => {
 
+
+    const { isLoading, error, setRequest, handleError } = useHttpClient();
+    const [loadedPlaces, setLoadedPlaces] = useState();
+
     const userId = useParams().userId;
-    const loadedPlaces = Dummy_Items.filter(place => place.creator === userId);
+
+
+    useEffect(() => {
+        const fetchPlace = async () => {
+            try {
+                const responceData = await setRequest(`http://localhost:8000/places/user/${userId}`);
+                setLoadedPlaces(responceData.place)
+            }
+            catch (err) { }
+
+        }
+        fetchPlace()
+    }, [setRequest, userId]);
+
+
     return (
-        <PlacesList
-            items={loadedPlaces}
-        />
+        <React.Fragment>
+            <ErrorModal error={error} onClear={handleError} /> {/* setting error from useState */}
+            {isLoading &&
+                <div className="center">
+                    <LoadSpinner asOverlay />
+                </div>
+            }
+            {!isLoading && loadedPlaces && <PlacesList items={loadedPlaces}/>}
+        </React.Fragment>
+
     );
 };
 
